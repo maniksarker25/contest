d9e2f6c3e115e3f480ed8a2070e9bf0873b56d1f
manik <maniksarker652@gmail.com>
2024-12-19 23:53:27 +0600

    successfully solved 4th problem

---
+++ contest-4/problem-4.cpp
@@ -0,0 +1,191 @@
+/*
+Problem Statement
+
+You have a singly linked list which is empty initially. Then you will be given Q queries. In each query you will be given two values X and V.
+
+If X is 0 that means you will insert the value V to the head of the linked list.
+If X is 1 then you will insert the value V to the tail of the linked list.
+If X is 2 then you will delete the value Vth index of the linked list. Assume that index starts from 0. If the index is invalid, then you shouldn't perform the deletion.
+After each query you need to print the linked list.
+Note: You must use singly linked list, otherwise you will not get marks.
+
+Input Format
+
+First line will contain Q.
+Next Q lines will contain X and V.
+Constraints
+
+1 <= Q <= 1000;
+0 <= X <= 2;
+0 <= V <= 10^9
+Output Format
+
+For each query ouput the updated linked list.
+Sample Input 0
+
+4
+0 10
+1 20
+1 30
+0 40
+Sample Output 0
+
+10
+10 20
+10 20 30
+40 10 20 30
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_head(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    newNode->next = head;
+    head = newNode;
+}
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+}
+
+void delete_from_head(Node *&head)
+{
+
+    Node *deletedNode = head;
+    head = head->next;
+    delete deletedNode;
+}
+
+void delete_from_any(Node *&head, int idx)
+{
+    Node *temp = head;
+    for (int i = 1; i < idx; i++)
+    {
+        temp = temp->next;
+    }
+
+    Node *deletedNode = temp->next;
+    temp->next = temp->next->next;
+    delete deletedNode;
+}
+
+void delete_from_tail(Node *&head, Node *&tail, int idx)
+{
+    Node *temp = head;
+    for (int i = 1; i < idx; i++)
+    {
+        temp = temp->next;
+    }
+
+    Node *deletedNode = temp->next;
+    temp->next = temp->next->next;
+    delete deletedNode;
+    tail = temp;
+}
+
+int count_linked_list(Node *head)
+{
+    Node *temp = head;
+    int count = 0;
+    while (temp != NULL)
+    {
+        temp = temp->next;
+        count++;
+    }
+
+    return count;
+}
+
+int main()
+{
+
+    Node *head = NULL;
+    Node *tail = NULL;
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; i++)
+    {
+        int x, v;
+        cin >> x >> v;
+        if (x == 0)
+        {
+            insert_at_head(head, tail, v);
+            print_linked_list(head);
+            cout << endl;
+        }
+        else if (x == 1)
+        {
+            insert_at_tail(head, tail, v);
+            print_linked_list(head);
+            cout << endl;
+        }
+        else if (x == 2)
+        {
+            int totalNode = count_linked_list(head);
+            if (v >= totalNode)
+            {
+                print_linked_list(head);
+                cout << endl;
+            }
+            else if (v == 0)
+            {
+                delete_from_head(head);
+                print_linked_list(head);
+                cout << endl;
+            }
+            else if (v == (totalNode - 1))
+            {
+                delete_from_tail(head, tail, v);
+                print_linked_list(head);
+                cout << endl;
+            }
+            else
+            {
+                delete_from_any(head, v);
+                print_linked_list(head);
+                cout << endl;
+            }
+        }
+    }
+    return 0;
+}
