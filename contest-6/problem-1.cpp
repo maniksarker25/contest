2acbfd9288be8da119fea5c48db2339a9ede7517
manik <maniksarker652@gmail.com>
2025-01-09 20:55:15 +0600

    successfully solved first problem

--- contest-6/problem-1.cpp
+++ contest-6/problem-1.cpp
@@ -4,6 +4,47 @@ using namespace std;
 
 int main()
 {
-    cout << "nice";
+    int n, m;
+    cin >> n >> m;
+    stack<int> st;
+    queue<int> q;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        st.push(x);
+    }
+    for (int i = 0; i < m; i++)
+    {
+        int x;
+        cin >> x;
+        q.push(x);
+    }
+    if (st.size() != q.size())
+    {
+        cout << "NO";
+        return 0;
+    }
+    int isSame = 1;
+
+    while (!st.empty())
+    {
+        if (st.top() != q.front())
+        {
+            isSame = 0;
+            break;
+        }
+        st.pop();
+        q.pop();
+    }
+    if (isSame == 1)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+
     return 0;
 }
