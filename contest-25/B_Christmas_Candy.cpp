9b08f30053f37144a473b383f62ce928d0babf60
manik <maniksarker652@gmail.com>
2025-06-02 20:59:25 +0600

    solve another problem

--- contest-25/B_Christmas_Candy.cpp
+++ contest-25/B_Christmas_Candy.cpp
@@ -11,18 +11,18 @@ int main()
     while(t--){
         int n;
         cin>>n;
-        vector<int>a(n);
+        int mx = 0,x,count = 0;
         for(int i = 0;i<n;i++){
-            cin >> a[i];
-        }
-
-        int count = 0;
-        for(int i = 0;i<n;i++){
-            if(i <n-1 && a[i] > a[i+1]){
+            cin >>x;
+            if(x > mx){
+                mx =x;
+            }
+            else{
                 count++;
             }
         }
         cout << count <<endl;
+        
     }
     
     return 0;
